ad6d41321f90472673a2568e269b2f24e2ee9ef2
Aakash927 <76126632+Aakash927@users.noreply.github.com>
2021-11-03 17:28:35 +0530

    Update clienthandler.java
    Added auction rules

--- clienthandler.java
+++ clienthandler.java
@@ -19,18 +19,25 @@ public class clienthandler implements Runnable {
     public void run() {
         // TODO Auto-generated method stub
         out.println("welome to the auction");
+        out.println("enter your name pls");
         out.flush();
         
-        
+    out.println("following are the rules for the auction, please read them carefully");
+    out.println(" ");
+    out.println("1 "+ "The auction will start after all the bidders have joined");
+    out.println("2 "+ "Each bidder will recieve 60 seconds to bid, a timer will be displayed for the same");
+    out.println("3 "+ "The bidder shall not bid lesser than the previous bid, any such bid will be considered invalid");
+    out.println("4 "+ "In case the bidder chooses not to bid for the item presented, just wait for the timer to run out");
+    out.println("5 "+ "There will be 10 rounds in all for all the bidders, the last bid will be the maximum and hence the respective bidder would be declared a winner");
+    out.println("6 "+ "A base price would be set for the item by the organiser, the bid shall be added to base price to make a total");
+    out.flush();
        
         try {
-            out.println("enter your name pls");
-            out.flush();
+            
+            //out.flush();
             String z = in.readLine();
 
             for(int j =0; j<200; j++){
-                
-
                 
                 String a = in.readLine();
                 int i=Integer.parseInt(a); 
